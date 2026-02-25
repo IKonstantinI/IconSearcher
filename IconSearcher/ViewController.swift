8f42b65376d62d54b9c816584173602f41fa0e86
Diel <diel404@gmoal.com>
2026-02-25 22:02:43 +0300

    Initial Commit

---
+++ IconSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IconSearcher
+//
+//  Created by Константин on 25.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
